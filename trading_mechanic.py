e2050f8c38bee93c38b2191f2c55e80a2bf4a308
Sber trading bot alive <max-256@inbox.ru>
2024-04-26 18:52:10 +0300

    add dependencies

--- trading_mechanic.py
+++ trading_mechanic.py
@@ -7,8 +7,12 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
-
-
+print('input the interval')
+b = int(input())
+print('input the price increase')
+increase = float(input())
+print('input the price decrease')
+decrease = float(input())
 
 FIGI = 'BBG004730N88'
 INSTRUMENT = 'e6123145-9665-43e0-8413-cd61b8aa9b13'
@@ -20,13 +24,14 @@ INSTRUMENT = 'e6123145-9665-43e0-8413-cd61b8aa9b13'
 
 
 
-
 a = True
 while a:
-    time.sleep(3)
+    time.sleep(b)
     get_candles.last_price()
+    ops_list = get_candles.get_orders()
     operations_obj = get_portfolio.get_ops()
     print(operations_obj)
+    print(ops_list.orders == [])
     rub_sv = operations_obj[0].units
     kop_sv = operations_obj[0].nano
     price_order = 0
@@ -34,11 +39,15 @@ while a:
     rub: int = get_candles.rub
     kop: float = get_candles.kop
     d = rub + (kop*0.000000001)
-    if operations_obj[1] == OperationType.OPERATION_TYPE_BUY and price_order + ((price_order/100)*0.9) <= d:
+    if operations_obj[1] == OperationType.OPERATION_TYPE_BUY and price_order + ((price_order/100)*increase) <= d and ops_list.orders == []:
         get_candles.order_send_sell()
-    elif operations_obj[1] == OperationType.OPERATION_TYPE_SELL and price_order - ((price_order/100)*0.9) >= d:
+        print('limit order sent, type: SELL')
+    elif operations_obj[1] == OperationType.OPERATION_TYPE_SELL and price_order - ((price_order/100)*decrease) >= d and ops_list.orders == []:
         get_candles.order_send()
+        print('limit order sent, type: BUY')
     else:
         print('last operation: ', operations_obj[1])
         print('last price: ', d)
         print('last operation price: ', price_order)
+        print('price to reach in case of growth: ', price_order + ((price_order/100)*increase))
+        print('price to reach in case of fall: ', price_order - ((price_order/100)*decrease))
